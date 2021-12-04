fcdb5fb6c37fa1be5206174cf54b1b4329c46d8e
user <favas@uitoux.in>
2021-12-04 20:23:33 +0530

    first

---
+++ android/app/src/main/java/com/project/flutter_app_navigate/MainActivity.java
@@ -0,0 +1,6 @@
+package com.project.flutter_app_navigate;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/DashboardB.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_navigate/CouraselWidget10.dart';
+import 'package:flutter_app_navigate/provider/provider.dart';
+import 'package:provider/provider.dart';
+
+
+
+
+
+class Dashboard extends ViewModelBuilderWidge
+{
+
+}
